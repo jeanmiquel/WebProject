069662709a029ad90ed03f157aa991b44839ed72
Deviljushiro <jean.miquel@live.fr>
2016-05-08 17:53:39 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-<footer>@Copyright Jean Miquel - 2016</footer>
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="view/js/bootstrap.min.js"></script>
