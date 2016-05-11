7e8f3434e247cf02112d03195fb6ba7d0111efce
Jean Miquel <jean.miquel@live.fr>
2016-05-11 23:59:27 +0200

    set new folder

--- controller/disconnection.php
+++ controller/disconnection.php
@@ -7,13 +7,13 @@
 
 //Update the time value of all the cookies and set them to a negative value to delete them
 
-setcookie("pseudo",NULL,-1, "/WebProject-masterV1/");
-setcookie("password","",-1, "/WebProject-masterV1/");
-setcookie("id","",-1, "/WebProject-masterV1/");
-setcookie("mail","",-1, "/WebProject-masterV1/");
-setcookie("lastname","",-1, "/WebProject-masterV1/");
-setcookie("firstname","",-1, "/WebProject-masterV1/");
-setcookie("status","",-1, "/WebProject-masterV1/");
+setcookie("pseudo",NULL,-1, "/WebProject/");
+setcookie("password","",-1, "/WebProject/");
+setcookie("id","",-1, "/WebProject/");
+setcookie("mail","",-1, "/WebProject/");
+setcookie("lastname","",-1, "/WebProject/");
+setcookie("firstname","",-1, "/WebProject/");
+setcookie("status","",-1, "/WebProject/");
 
 header('Location: ../view/index.php');
 
